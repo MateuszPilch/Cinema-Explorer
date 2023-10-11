f2cb39f65718399ada8aee2c22af27ad8e118fbb
Mati <skapuni@gmail.com>
2023-10-11 12:53:14 +0200

    movie page added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { MoviePageComponent } from './movies/movie-page/movie-page.component';
+import { MapPageComponent } from './maps/map-page/map-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    MoviePageComponent,
+    MapPageComponent
   ],
   imports: [
     BrowserModule,
