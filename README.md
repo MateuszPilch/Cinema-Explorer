9e44fd5af91ecd09309fcb6de754996e6713e62f
Mati <skapuni@gmail.com>
2025-03-04 00:10:23 +0100

    frontend project name changed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PracaInzynierska
+# Cinema explorer
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.7.
 
--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'praca-inzynierska'`, () => {
+  it(`should have as title 'cinema-explorer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('praca-inzynierska');
+    expect(app.title).toEqual('cinema-explorer');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('praca-inzynierska app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cinema-explorer app is running!');
   });
 });
