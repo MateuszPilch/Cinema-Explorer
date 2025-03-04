653034927ceea430ff6e6f4ff73e83501eca157f
Mati <skapuni@gmail.com>
2025-03-04 16:01:56 +0100

    endpoints fixing

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:4200', process.env.FRONTEND_URL],
+    credentials: true
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
